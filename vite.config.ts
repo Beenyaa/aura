d764b09089e235e278fc94b01025e623985e97f5
Beenyaa <bencegadanyi1@hotmail.com>
2026-01-07 01:16:38 +0000

    chore: deployable

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
-	base: "/aura/",
+  plugins: [react()],
+  base: "/aura/",
+  define: {
+    "process.env": {},
+  },
 });
